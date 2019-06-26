972a96c38838cb32e8981a44f64dac14382f0d79
OOsiriSS <46490535+OOsiriSS@users.noreply.github.com>
2019-06-26 23:22:47 +0800

    Update and rename Mvp0Contract.java to Mvp3Contract.java

--- app/src/main/java/nju/androidchat/client/hw1/Mvp3Contract.java
+++ app/src/main/java/nju/androidchat/client/hw1/Mvp3Contract.java
@@ -1,28 +1,30 @@
-package nju.androidchat.client.mvp0;
+package nju.androidchat.client.hw1;
 
 import java.util.List;
+import java.util.UUID;
 
 import nju.androidchat.client.ClientMessage;
 
-public interface Mvp0Contract {
-    interface View extends BaseView<Presenter> {
+public interface Mvp3Contract {
+    interface TalkView extends BaseView<TalkPresenter> {
         void showMessageList(List<ClientMessage> messages);
     }
 
-    interface Presenter extends BasePresenter {
+    interface TalkPresenter extends BasePresenter {
         void sendMessage(String content);
 
         void receiveMessage(ClientMessage content);
 
         String getUsername();
 
-        //撤回消息mvp0不实现
-        void recallMessage(int index0);
+        void recallMessage(UUID messageId);
     }
 
-    interface Model {
+    interface TalkModel {
         ClientMessage sendInformation(String message);
 
         String getUsername();
+
+        void recallMessage(UUID messageId);
     }
 }
